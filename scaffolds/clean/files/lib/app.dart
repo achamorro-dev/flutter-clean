8704319a86cc565f11b3110f8528f20742603fc5
Alberto Chamorro <a.chamorro.ruiz@gmail.com>
2021-03-14 09:08:23 +0100

    Added theme to test

--- scaffolds/clean/files/lib/app.dart
+++ scaffolds/clean/files/lib/app.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import 'package:@appName/theme/theme.dart';
 class @AppNameApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         title: '@appName',
+        theme: theme,
         navigatorKey: AppNavigator.navigatorKey,
         initialRoute: '/',
         onGenerateRoute: RouteGenerator.generateRouter,
---
+++ scaffolds/clean/files/lib/theme/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+final theme = ThemeData(
+  primaryColorDark: const Color(0xFF000000),
+  primaryColorLight: const Color(0xFFffffff),
+  primaryColor: const Color(0xFFffffff),
+  accentColor: const Color(0xFF3F21FF),
+  scaffoldBackgroundColor: const Color(0xFFffffff),
+  inputDecorationTheme: InputDecorationTheme(
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(8),
+    ),
+  ),
+);
